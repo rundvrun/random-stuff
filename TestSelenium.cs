bf83aacb0ba38b0cac41d0e764535581d8242c2b
rundvrun <rundvrun@gmail.com>
2024-10-03 08:59:13 +0700

    C# selenium Android test

---
+++ TestSelenium.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace TestSeleniumDriver
+{
+    class TestSelenium
+    {
+        [Test]
+        public static void TC01()
+        {
+            var driver = new FirefoxDriver();
+            string baseUrl = "https://demo.opencart.com/";
+            driver.Navigate().GoToUrl(baseUrl);
+            var btn43 = driver.FindElement(By.XPath("//button[contains(@onclick, '43')]"));
+            Assert.AreEqual("cart.add('43');", btn43.GetAttribute("onclick"));
+            btn43.Click();
+        }
+    }
+}

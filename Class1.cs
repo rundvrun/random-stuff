6e95fccbdd3b75117a78e10619a365ca424eb665
rundvrun <rundvrun@gmail.com>
2024-10-03 09:00:07 +0700

    another selenium c# homework

---
+++ Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+
+namespace SeleniumTest
+{
+    public static class Env
+    {
+        public static String rootDirectory = System.IO.Path.GetFullPath($"{System.AppDomain.CurrentDomain.BaseDirectory.ToString()}/../../../..");
+
+        static public bool IsSauce()
+        {
+            return Environment.GetEnvironmentVariable("SAUCE_LABS") != null;
+        }
+
+        static public Uri ServerUri()
+        {
+            String sauceUserName = Environment.GetEnvironmentVariable("SAUCE_USERNAME");
+            String sauceAccessKey = Environment.GetEnvironmentVariable("SAUCE_ACCESS_KEY");
+
+            return (sauceUserName == null) || (sauceAccessKey == null)
+                ? new Uri("http://localhost:4723/wd/hub")
+                : new Uri($"https://{sauceUserName}:{sauceAccessKey}@ondemand.saucelabs.com:80/wd/hub");
+        }
+
+        public static TimeSpan INIT_TIMEOUT_SEC = TimeSpan.FromSeconds(180);
+        public static TimeSpan IMPLICIT_TIMEOUT_SEC = TimeSpan.FromSeconds(10);
+    }
+    [TestFixture()]
+    public class Class1
+    {
+        static AndroidDriver<AndroidElement> driver;
+        [SetUp()]
+        public static void SetUp()
+        {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.SetCapability(MobileCapabilityType.BrowserName, "");
+            capabilities.SetCapability(MobileCapabilityType.PlatformName, "Android");
+            capabilities.SetCapability(MobileCapabilityType.PlatformVersion, "5.1");
+            capabilities.SetCapability(MobileCapabilityType.AutomationName, "UIAutomator2");
+            capabilities.SetCapability(MobileCapabilityType.DeviceName, "Galaxy J1");
+            //capabilities.SetCapability("appActivity", ".app.SearchInvoke");
+            capabilities.SetCapability(MobileCapabilityType.App, "F:\\ApiDemos-debug.apk");
+
+            driver = new AndroidDriver<AndroidElement>(Env.ServerUri(), capabilities, Env.INIT_TIMEOUT_SEC);
+            driver.Manage().Timeouts().ImplicitlyWait(Env.IMPLICIT_TIMEOUT_SEC);
+        }
+        [TearDown()]
+        public static void TearDown()
+        {
+            driver.Quit();
+        }
+
+        public static AndroidElement ScrollToElement(string elementText)
+        {
+            return driver.FindElementByAndroidUIAutomator($"new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"{elementText}\").instance(0))");
+        }
+
+        [Test]
+        public void TestLogTextBoxAdd()
+        {
+            ScrollToElement("Text").Click();
+            driver.FindElementByAccessibilityId("LogTextBox").Click();
+            driver.FindElementByAccessibilityId("Add").Click();
+            var txtBox = driver.FindElementById("io.appium.android.apis:id/text");
+            Assert.IsNotEmpty(txtBox.Text);
+            var old = txtBox.Text;
+            driver.FindElementByAccessibilityId("Do nothing").Click();
+            Assert.AreEqual(old, driver.FindElementById("io.appium.android.apis:id/text").Text);
+        }
+        [Test]
+        public void TestViewFlip()
+        {
+            driver.FindElementByAccessibilityId("Animation").Click();
+            ScrollToElement("View Flip").Click();
+            var elemXpath = "/hierarchy/android.widget.FrameLayout/android.view.View/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ListView/android.widget.TextView[1]";
+            Assert.AreEqual("One", driver.FindElementByXPath(elemXpath).Text);
+            driver.FindElementByAccessibilityId("Flip").Click();
+            Assert.AreEqual("Un", driver.FindElementByXPath(elemXpath).Text);
+            driver.FindElementByAccessibilityId("Flip").Click();
+            Assert.AreEqual("One", driver.FindElementByXPath(elemXpath).Text);
+        }
+        [Test]
+        public void TestCustomTitle()
+        {
+            driver.FindElementByAccessibilityId("App").Click();
+            driver.FindElementByAccessibilityId("Activity").Click();
+            driver.FindElementByAccessibilityId("Custom Title").Click();
+
+            var leftTxtBox = driver.FindElementById("io.appium.android.apis:id/left_text_edit");
+            var leftTxtBtn = driver.FindElementById("io.appium.android.apis:id/left_text_button");
+            leftTxtBox.Clear();
+            leftTxtBox.SendKeys("Hello");
+            leftTxtBtn.Click();
+            var leftTxt = driver.FindElementById("io.appium.android.apis:id/left_text");
+            Assert.AreEqual("Hello", leftTxt.Text);
+
+            var rightTxtBox = driver.FindElementById("io.appium.android.apis:id/right_text_edit");
+            var rightTxtBtn = driver.FindElementById("io.appium.android.apis:id/right_text_button");
+            rightTxtBox.Clear();
+            rightTxtBox.SendKeys("World");
+            rightTxtBtn.Click();
+            var rightTxt = driver.FindElementById("io.appium.android.apis:id/right_text");
+            Assert.AreEqual("World", rightTxt.Text);
+        }
+    }
+}
---
+++ DemoOpenCartSeleniumTest.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class DemoOpenCartSeleniumTest
+    {
+        FirefoxDriver driver;
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new FirefoxDriver();
+            var baseUrl = "https://demo.opencart.com/";
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+
+        [TestMethod, Description("Input: Page, Output: Init cart total = $0.00"), Timeout(20000)]
+        public void TC_EmptyCart_Message()
+        {
+            var element = driver.FindElementByCssSelector("#cart-total");
+            Assert.IsNotNull(element);
+            Assert.AreEqual("0 item(s) - $0.00", element.Text);
+        }
+        [TestMethod, Description("Input: Phones & PDAs, Output: 3"), Timeout(20000)]
+        public void TC_Number_Of_Product()
+        {
+            driver.FindElementByXPath("//a[text() = 'Phones & PDAs']").Click();
+            Thread.Sleep(1200);
+            var text = driver.FindElementByCssSelector(".list-group-item.active").Text;
+            var products = driver.FindElementsByClassName("product-thumb");
+            var product_cate = driver.FindElementByCssSelector("#content>h2").Text;
+            Assert.AreEqual($"{product_cate} ({products.Count})", text);
+        }
+        [TestMethod, Description("Input: Add 1 item (43), Output: 1 item(s) in cart ($602.00)"), Timeout(20000)]
+        public void TC_Add_1_Item()
+        {
+            var button = driver.FindElementByCssSelector("button[onclick=\"cart.add('43');\"]");
+            Assert.IsNotNull(button);
+            button.Click();
+            Thread.Sleep(1000);
+            var element = driver.FindElementByCssSelector("#cart-total");
+            Assert.IsNotNull(element);
+            Assert.AreEqual("1 item(s) - $602.00", element.Text);
+        }
+        [TestMethod, Description("Input: 1-item cart, Output: Empty cart"), Timeout(20000)]
+        public void TC_Remove_1_Item()
+        {
+            var button = driver.FindElementByCssSelector("button[onclick=\"cart.add('43');\"]");
+            Assert.IsNotNull(button);
+            button.Click();
+            Thread.Sleep(1000);
+            var cart_button = driver.FindElementByCssSelector("#cart>button");
+            cart_button.Click();
+            Thread.Sleep(1000);
+            var remove_button = driver.FindElementByCssSelector("button[title='Remove']");
+            Assert.IsNotNull(remove_button);
+            remove_button.Click();
+            Thread.Sleep(1500);
+            cart_button.Click();
+            Thread.Sleep(1000);
+            var text = driver.FindElementByCssSelector("#cart>ul>li>p").Text;
+            Assert.AreEqual("Your shopping cart is empty!", text);
+        }
+        [TestMethod, Description("Input: Empty Category (Software), Output: There are no products to list in this category."), Timeout(20000)]
+        public void TC_Category_0_Item()
+        {
+            driver.FindElementByXPath("//a[text() = 'Software']").Click();
+            Thread.Sleep(1200);
+            var text = driver.FindElementByCssSelector(".list-group-item.active").Text;
+            Assert.IsTrue(text.Contains("(0)"));
+            var message = driver.FindElementByCssSelector("#content>p").Text;
+            Assert.AreEqual("There are no products to list in this category.", message);
+        }
+        [TestMethod, Description("Input: Phones & PDAs, Output: Sort Descending By Price"), Timeout(20000)]
+        public void TC_Desc_Price()
+        {
+            driver.FindElementByXPath("//a[text() = 'Phones & PDAs']").Click();
+            Thread.Sleep(1200);
+            new SelectElement(driver.FindElementById("input-sort")).SelectByText("Price (High > Low)");
+            var items = driver.FindElementsByClassName("price");
+            bool desc = true;
+            double curr_price = Double.MaxValue - 1;
+            var regex = new Regex(@"\$(\d+\.\d+)*");
+            foreach (var item in items)
+            {
+                double price = Convert.ToDouble(regex.Match(item.Text).Groups[1].Value);
+                if (price > curr_price)
+                {
+                    desc = false;
+                    break;
+                }
+                curr_price = price;
+            }
+            Assert.IsTrue(desc);
+        }
+        [TestMethod, Description("Input: Phones & PDAs, Output: Sort Ascending By Name"), Timeout(20000)]
+        public void TC_Asc_Name()
+        {
+            driver.FindElementByXPath("//a[text() = 'Phones & PDAs']").Click();
+            Thread.Sleep(1200);
+            new SelectElement(driver.FindElementById("input-sort")).SelectByText("Name (A - Z)");
+            var items = driver.FindElementsByCssSelector(".caption>h4>a");
+            bool asc = true;
+            for (int i = 1; asc && i < items.Count; ++i) 
+                asc = String.Compare(items[i - 1].Text, items[i].Text) <= 0;
+            Assert.IsTrue(asc);
+        }
+        [TestMethod, Description("Input: 19-item page, Output: There are no more 25 items in page"), Timeout(20000)]
+        public void TC_List_Limit_25()
+        {
+            driver.FindElementByName("search").SendKeys(" ");
+            driver.FindElementByCssSelector(".input-group-btn > button").Click();
+            new SelectElement(driver.FindElementById("input-limit")).SelectByText("25");
+            Assert.IsTrue(25 >= driver.FindElementsByClassName("product-thumb").Count);
+        }
+        [TestMethod, Description("Input: 19-item page, Output: 15-item page (limit)"), Timeout(20000)]
+        public void TC_List_Limit_15()
+        {
+            driver.FindElementByName("search").SendKeys(" ");
+            driver.FindElementByCssSelector(".input-group-btn > button").Click();
+            new SelectElement(driver.FindElementById("input-limit")).SelectByText("15");
+            Assert.AreEqual(15, driver.FindElementsByClassName("product-thumb").Count);
+        }
+        [TestMethod, Description("Input: Change currency to EUR, Output: Currency is €"), Timeout(20000)]
+        public void TC_Change_Currency_To_EUR()
+        {
+            driver.FindElementByCssSelector("#form-currency > .btn-group > button").Click();
+            driver.FindElementByName("EUR").Click();
+            driver.FindElementByName("search").SendKeys(" ");
+            driver.FindElementByCssSelector(".input-group-btn > button").Click();
+            new SelectElement(driver.FindElementById("input-limit")).SelectByText("50");
+            bool isChanged = true;
+            foreach (var item in driver.FindElementsByClassName("price"))
+            {
+                if (!item.Text.Contains("€"))
+                {
+                    isChanged = false;
+                    break;
+                }
+            }
+            Assert.IsTrue(isChanged);
+        }
+    }
+}
